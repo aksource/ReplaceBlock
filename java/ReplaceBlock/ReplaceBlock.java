c39cb74f7a6bf31863ace41333c59bf3c1398a05
aksource <akira.mew@gmail.com>
2014-06-19 20:51:18 +0900

    mod version changeable from build.gradle file.

--- java/ReplaceBlock/ReplaceBlock.java
+++ java/ReplaceBlock/ReplaceBlock.java
@@ -16,7 +16,7 @@ import cpw.mods.fml.common.registry.GameData;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.GameRegistry.UniqueIdentifier;
 
-@Mod(modid="ReplaceBlock", name="ReplaceBlock", version="1.7srg-1",dependencies="required-after:FML", useMetadata = true)
+@Mod(modid="ReplaceBlock", name="ReplaceBlock", version="@VERSION@",dependencies="required-after:FML", useMetadata = true)
 public class ReplaceBlock
 {
 	@Mod.Instance("ReplaceBlock")
