ff3f2b6f3abd1ef07a4681fc4852787cf7fbc92e
aksource <akira.mew@gmail.com>
2016-06-16 23:34:34 +0900

    test

--- java/ReplaceBlock/ReplaceBlock.java
+++ java/ReplaceBlock/ReplaceBlock.java
@@ -17,99 +17,95 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import java.util.ArrayList;
 
-@Mod(modid="ReplaceBlock", name="ReplaceBlock", version="@VERSION@",dependencies="required-after:FML", useMetadata = true)
-public class ReplaceBlock
-{
-	@Mod.Instance("ReplaceBlock")
-	public static ReplaceBlock instance;
+@Mod(modid = ReplaceBlock.MOD_ID,
+        name = ReplaceBlock.MOD_NAME,
+        version = ReplaceBlock.MOD_VERSION,
+        dependencies = ReplaceBlock.MOD_DEPENDENCIES,
+        useMetadata = true,
+        acceptedMinecraftVersions = ReplaceBlock.MOD_MC_VERSION)
+public class ReplaceBlock {
+    public static final String MOD_ID = "ReplaceBlock";
+    public static final String MOD_NAME = "ReplaceBlock";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String MOD_DEPENDENCIES = "required-after:Forge@[11.14.0.1237,)";
+    public static final String MOD_MC_VERSION = "[1.8,1.8.9]";
 
-	public static String[] targetBlockID;
-	public static String replaceBlockID;
-	public static int targetYposMax;
-	public static int targetYposMin;
-	public static int chunkWidely;
+    public static String[] targetBlockID;
+    public static String replaceBlockID;
+    public static int targetYposMax;
+    public static int targetYposMin;
+    public static int chunkWidely;
 
-	private Chunk lastChunk = null;
-	private int chunkWide = 16;
-	protected ArrayList<Integer> ids = new ArrayList<Integer>();
+    private Chunk lastChunk = null;
+    private static final int CHUNK_WIDE = 16;
+    protected ArrayList<Integer> ids = new ArrayList<Integer>();
 
-	public class LivingUpdateHook
-	{
-		@SubscribeEvent
-		public void LivingUpdate(LivingUpdateEvent event)
-		{
-			if(!event.entityLiving.worldObj.isRemote)
-			{
-				int posX = (int)Math.floor(event.entityLiving.posX);
-				int posZ = (int)Math.floor(event.entityLiving.posZ);
-				int posY = (int)Math.floor(event.entityLiving.posY);
-				Chunk chunk = event.entityLiving.worldObj.getChunkFromBlockCoords(new BlockPos(event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ));
-				if(chunk != lastChunk && targetYposMax+chunkWidely >= posY && posY >= targetYposMin-chunkWidely)
-				{
-					lastChunk = chunk;
-					int chunkX = chunk.xPosition * chunkWide;
-					int chunkZ = chunk.zPosition * chunkWide;
-					for(int x = chunkX-chunkWidely; x < chunkX + chunkWide+chunkWidely; x++){
-						for(int z = chunkZ-chunkWidely; z < chunkZ + chunkWide+chunkWidely; z++)
-						{
-							for(int y = targetYposMax; y >= targetYposMin; y--)
-							{
+    public class LivingUpdateHook {
+        @SubscribeEvent
+        public void LivingUpdate(LivingUpdateEvent event) {
+            if (!event.entityLiving.worldObj.isRemote) {
+                int posX = (int) Math.floor(event.entityLiving.posX);
+                int posZ = (int) Math.floor(event.entityLiving.posZ);
+                int posY = (int) Math.floor(event.entityLiving.posY);
+                Chunk chunk = event.entityLiving.worldObj.getChunkFromBlockCoords(new BlockPos(event.entityLiving.posX, event.entityLiving.posY, event.entityLiving.posZ));
+                if (chunk != lastChunk && targetYposMax + chunkWidely >= posY && posY >= targetYposMin - chunkWidely) {
+                    lastChunk = chunk;
+                    int chunkX = chunk.xPosition * CHUNK_WIDE;
+                    int chunkZ = chunk.zPosition * CHUNK_WIDE;
+                    for (int x = chunkX - chunkWidely; x < chunkX + CHUNK_WIDE + chunkWidely; x++) {
+                        for (int z = chunkZ - chunkWidely; z < chunkZ + CHUNK_WIDE + chunkWidely; z++) {
+                            for (int y = targetYposMax; y >= targetYposMin; y--) {
                                 BlockPos blockPos = new BlockPos(x, y, z);
-								if(this.isTargetBlock(event.entityLiving.worldObj.getBlockState(blockPos).getBlock()))
-								{
-									//minecraft.getIntegratedServer().worldServerForDimension(minecraft.thePlayer.dimension).setBlock(x, y, z, replaceBlockID, 0, 3);
-									Block block = GameData.getBlockRegistry().getObject(replaceBlockID);
-									event.entityLiving.worldObj.setBlockState(blockPos, block.getDefaultState());
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		public boolean isTargetBlock(Block block)
-		{
-			for(int i = 0; i < targetBlockID.length; i++)
-			{
-				if(targetBlockID[i].equals(this.getUniqueStrings(block)))
-				{
-					return true;
-				}
-			}
-			return false;
-		}
-		public String getUniqueStrings(Object obj)
-		{
-			GameRegistry.UniqueIdentifier uId;
-			if(obj instanceof Block) {
-				uId = GameRegistry.findUniqueIdentifierFor((Block) obj);
-			}else {
-				uId = GameRegistry.findUniqueIdentifierFor((Item) obj);
-			}
-			return Optional.fromNullable(uId).or(new GameRegistry.UniqueIdentifier("none:dummy")).toString();
+                                if (this.isTargetBlock(event.entityLiving.worldObj.getBlockState(blockPos).getBlock())) {
+                                    //minecraft.getIntegratedServer().worldServerForDimension(minecraft.thePlayer.dimension).setBlock(x, y, z, replaceBlockID, 0, 3);
+                                    Block block = GameData.getBlockRegistry().getObject(replaceBlockID);
+                                    event.entityLiving.worldObj.setBlockState(blockPos, block.getDefaultState());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-		}
-	}
+        public boolean isTargetBlock(Block block) {
+            for (int i = 0; i < targetBlockID.length; i++) {
+                if (targetBlockID[i].equals(this.getUniqueStrings(block))) {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event)
-	{
-		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		config.load();
-		targetBlockID = config.get(Configuration.CATEGORY_GENERAL, "targetBlockNames", new String[]{"minecraft:dirt", "minecraft:bedrock", "minecraft:gravel"}).getStringList();
-		replaceBlockID = config.get(Configuration.CATEGORY_GENERAL, "replaceBlockName", "minecraft:stone", "ReplaceBlockName: (format)ModId:UniqueName").getString();
-		targetYposMax = config.get(Configuration.CATEGORY_GENERAL, "targetYposMax", 20, "targetYposMax,min=1,max=255").getInt();
-		targetYposMax = (targetYposMax < 1)?1:(targetYposMax > 255)?255:targetYposMax;
-		targetYposMin = config.get(Configuration.CATEGORY_GENERAL, "targetYposMin", 1, "targetYposMin,min=1,max=255").getInt();
-		targetYposMin = (targetYposMin < 1)?1:(targetYposMin > targetYposMax)?targetYposMax:targetYposMin;
-		chunkWidely = config.get(Configuration.CATEGORY_GENERAL, "chunkWidely", 3, "chankWidely,min=0,max=16").getInt();
-		chunkWidely = (chunkWidely < 0)?0:(chunkWidely > 16)?16:chunkWidely;
-		config.save();
-	}
+        public String getUniqueStrings(Object obj) {
+            GameRegistry.UniqueIdentifier uId;
+            if (obj instanceof Block) {
+                uId = GameRegistry.findUniqueIdentifierFor((Block) obj);
+            } else {
+                uId = GameRegistry.findUniqueIdentifierFor((Item) obj);
+            }
+            return Optional.fromNullable(uId).or(new GameRegistry.UniqueIdentifier("none:dummy")).toString();
 
-	@Mod.EventHandler
-	public void load(FMLInitializationEvent event)
-	{
-		MinecraftForge.EVENT_BUS.register(new LivingUpdateHook());
-	}
+        }
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+        config.load();
+        targetBlockID = config.get(Configuration.CATEGORY_GENERAL, "targetBlockNames", new String[]{"minecraft:dirt", "minecraft:bedrock", "minecraft:gravel"}).getStringList();
+        replaceBlockID = config.get(Configuration.CATEGORY_GENERAL, "replaceBlockName", "minecraft:stone", "ReplaceBlockName: (format)ModId:UniqueName").getString();
+        targetYposMax = config.get(Configuration.CATEGORY_GENERAL, "targetYposMax", 20, "targetYposMax,min=1,max=255").getInt();
+        targetYposMax = (targetYposMax < 1) ? 1 : (targetYposMax > 255) ? 255 : targetYposMax;
+        targetYposMin = config.get(Configuration.CATEGORY_GENERAL, "targetYposMin", 1, "targetYposMin,min=1,max=255").getInt();
+        targetYposMin = (targetYposMin < 1) ? 1 : (targetYposMin > targetYposMax) ? targetYposMax : targetYposMin;
+        chunkWidely = config.get(Configuration.CATEGORY_GENERAL, "chunkWidely", 3, "chankWidely,min=0,max=16").getInt();
+        chunkWidely = (chunkWidely < 0) ? 0 : (chunkWidely > 16) ? 16 : chunkWidely;
+        config.save();
+    }
+
+    @Mod.EventHandler
+    public void load(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new LivingUpdateHook());
+    }
 }
